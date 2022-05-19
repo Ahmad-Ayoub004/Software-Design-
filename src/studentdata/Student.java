b896d9a1f9b37d20f59bd89d578b481ab59c3dbc
Ahmad <ayoubah@sheridancollege.ca>
2022-05-19 11:39:16 -0400

    refactored code by providing getters and setters to progName

--- src/studentdata/Student.java
+++ src/studentdata/Student.java
@@ -23,5 +23,19 @@ public class Student {
     public void setStudentName(String studentName){
         this.studentName = studentName;
     }
+
+    /**
+     * @return the progName
+     */
+    public String getProgName() {
+        return progName;
+    }
+
+    /**
+     * @param progName the progName to set
+     */
+    public void setProgName(String progName) {
+        this.progName = progName;
+    }
 }
 
