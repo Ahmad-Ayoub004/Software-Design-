155efce3b238a386707a61f4c1e71a34ffb80f0e
Ahmad <ayoubah@sheridancollege.ca>
2022-05-26 10:21:29 -0400

    refactored code by providing getters and setters to progName

---
+++ src/studentdata/PartTimeStudent.java
@@ -0,0 +1,11 @@
+package studentdata;
+
+/**
+ * This class +++ Insert class description here +++
+ *
+ * @author Ahmad Ayoub
+ */
+public class PartTimeStudent extends Student {
+    private int Student;
+
+}
